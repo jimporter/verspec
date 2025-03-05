d0c895fed94196abd320db3aff276d3e227ff398
Jim Porter <itsjimporter@gmail.com>
2025-03-04 16:44:34 -0800

    Fix CI and test against newer Python versions

--- setup.py
+++ setup.py
@@ -120,12 +120,13 @@ setup(
         "License :: OSI Approved :: BSD License",
 
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
 
     packages=find_packages(exclude=["test", "test.*"]),
